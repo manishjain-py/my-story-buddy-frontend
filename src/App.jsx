e1391d195a3bf5fb72481ebfaf6ee211b3f1c733
Manish Jain <manish_jain1@hotmail.com>
2025-06-23 20:12:33 +0530

    Update Lambda URL for production deployment

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ function AppContent() {
 
   const API_URL = window.location.hostname === 'localhost' 
     ? 'http://127.0.0.1:8003'
-    : 'https://e23mdrxxzglqosvp4maifljwky0mxabd.lambda-url.us-west-2.on.aws';
+    : 'https://plctvlxyx65kl2ygonimr6efa40zfunz.lambda-url.us-east-1.on.aws';
 
   // Handle OAuth callback
   useEffect(() => {
